cd3b051c8ce2efd6cebbc7be90362eaf3ead6a66
Akhil0507 <85498044+AkhilNuthi@users.noreply.github.com>
2023-09-23 03:20:44 +0530

    Update glassdoor_scraper.py

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  2 09:32:36 2020
-
-author: Kenarapfaik
-url: https://github.com/arapfaik/scraping-glassdoor-selenium
-"""
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium import webdriver
 import time
@@ -13,7 +6,7 @@ import pandas as pd
 
 def get_jobs(keyword, num_jobs, verbose, path, slp_time):
     
-    '''Gathers jobs as a dataframe, scraped from Glassdoor'''
+   
     
     #Initializing the webdriver
     options = webdriver.ChromeOptions()
@@ -26,7 +19,6 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
     driver.set_window_size(1120, 1000)
     
     url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword="+keyword+"&sc.keyword="+keyword+"&locT=&locId=&jobType="
-    #url = 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword="' + keyword + '"&locT=C&locId=1147401&locKeyword=San%20Francisco,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0'
     driver.get(url)
     jobs = []
 
